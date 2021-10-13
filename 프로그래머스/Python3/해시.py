5d2cddc7b41fc007bc6f73495424d840ee57e1d8
ChoiByeol <honey333888@naver.com>
2021-10-13 18:59:38 +0900

    [프로그래머스] 해시 - 전화번호 목록

--- 프로그래머스/Python3/해시.py
+++ 프로그래머스/Python3/해시.py
@@ -8,3 +8,13 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             answer = participant[i]
             return answer
+
+# 전화번호 목록
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(0, len(phone_book) - 1):
+        if (phone_book[i] == phone_book[i+1][:len(phone_book[i])]):
+            answer = False
+            break
+    return answer
