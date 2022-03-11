3c058528e2c3fc10305dfb0acae1d8ecfe0db787
ChoiByeol <honey3388@khu.ac.kr>
2022-03-11 16:26:04 +0900

    [프로그래머스] 정렬 - 가장 큰 수

--- 프로그래머스/Python3/정렬.py
+++ 프로그래머스/Python3/정렬.py
@@ -1,4 +1,4 @@
-# k번째수
+# k번째 수
 def solution(array, commands):
     answer = []
     list = []
@@ -10,3 +10,35 @@ def solution(array, commands):
         list.sort()
         answer.append(list[idx])
     return answer
+
+# 가장 큰 수
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x * 3, reverse=True)
+    answer = str(int(''.join(numbers)))
+    return answer
+    """
+    틀렸던 답
+    def solution(numbers):
+        answer = ''
+        temp = []
+        numbers = sorted(map(str, numbers), reverse=True)
+        i = 0
+        for num in numbers:
+            if '0' in num:
+                temp.append(num)
+            else:
+                answer += num
+            if i < len(numbers) - 1:
+                if num[0] != numbers[i+1][0]:
+                    temp.sort()
+                    for num in temp:
+                        answer += num
+                    temp = []
+            if i == len(numbers) - 1:
+                temp.sort()
+                for num in temp:
+                    answer += num
+            i += 1
+        return answer
+    """
