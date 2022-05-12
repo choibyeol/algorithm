9501601726aa6462ea4bb1ee30c993b6748d2eb5
ChoiByeol <honey3388@khu.ac.kr>
2022-05-13 04:15:56 +0900

    [프로그래머스] 해시 - 베스트앨범

--- 프로그래머스/Python3/해시.py
+++ 프로그래머스/Python3/해시.py
@@ -1,4 +1,4 @@
-# 완주하지 못한 선수
+# 완주하지 못한 선수 level 1
 def solution(participant, completion):
     answer = ''
     participant.sort()
@@ -9,7 +9,7 @@ def solution(participant, completion):
             answer = participant[i]
             return answer
 
-# 전화번호 목록
+# 전화번호 목록 level 2
 def solution(phone_book):
     answer = True
     phone_book.sort()
@@ -19,7 +19,7 @@ def solution(phone_book):
             break
     return answer
 
-# 위장
+# 위장 level 2
 def solution(clothes):
     answer = 1
     comb = {}
@@ -34,3 +34,34 @@ def solution(clothes):
         answer *= comb[key] + 1
             
     return answer - 1
+
+# 베스트앨범 level 3
+import operator
+
+def solution(genres, plays):
+    answer = []
+    genre_dic = {}
+    play_dic = {}
+    cnt = 0
+    for genre in genres:
+        genre_dic[genre] = 0
+        play_dic[genre] = []
+        
+    for genre in genres:
+        genre_dic[genre] += plays[cnt]
+        play_dic[genre].append([cnt, plays[cnt]])
+        cnt += 1
+    
+    genre_max = sorted(genre_dic.items(), key=operator.itemgetter(1), reverse=True)
+    for genre in genre_max:
+        play_max = []
+        print(genre[0])
+        for i in range(len(play_dic[genre[0]])):
+            play_max.append([play_dic[genre[0]][i][1], play_dic[genre[0]][i][0]])
+        play_max = sorted(play_max, key = lambda x : (-x[0], x[1]))
+        for i in range(len(play_max)):
+            answer.append(play_max[i][1])
+            if i == 1:
+                break
+    
+    return answer
