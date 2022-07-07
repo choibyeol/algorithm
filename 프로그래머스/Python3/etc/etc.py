7a97eb0a30a6a920e12296baa2877f947a7ccfac
ChoiByeol <honey333888@naver.com>
2022-07-07 21:37:21 +0900

    [프로그래머스] 음양 더하기

---
+++ 프로그래머스/Python3/etc/etc.py
@@ -0,0 +1,9 @@
+# 음양 더하기 level 1
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
