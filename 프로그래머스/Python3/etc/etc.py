1c769c958e21b9dde589bd1685e22b1b8abffa84
ChoiByeol <honey333888@naver.com>
2022-07-07 21:49:30 +0900

    [프로그래머스] 없는 숫자 더하기

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -31,3 +31,11 @@ def isPrime(num):
         if num % i == 0:
             return False
     return True
+
+# 없는 숫자 더하기 level 1
+def solution(numbers):
+    answer = 0
+    for i in range(1, 10):
+        if i not in numbers:
+            answer += i
+    return answer
