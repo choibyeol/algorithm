0691beb5b3e3febcf3fb8700c7256eff62e0d8d6
ChoiByeol <honey333888@naver.com>
2022-07-20 23:30:57 +0900

    [프로그래머스] 나머지가 1이 되는 수 찾기

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -138,3 +138,12 @@ def solution(d, budget):
             break
         answer += 1
     return answer
+
+# 나머지가 1이 되는 수 찾기 level 1
+def solution(n):
+    answer = 0
+    for i in range(1, n):
+        if n % i == 1:
+            answer = i
+            break
+    return answer
