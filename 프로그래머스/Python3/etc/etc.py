f521669b57e18e6134d23bc94b3d4519461da273
ChoiByeol <honey333888@naver.com>
2022-07-22 20:14:45 +0900

    [프로그래머스] 두 정수 사이의 합

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -169,3 +169,14 @@ def solution(arr, divisor):
         answer.append(-1)
     answer.sort()
     return answer
+
+# 두 정수 사이의 합 level 1
+def solution(a, b):
+    answer = 0
+    if a == b:
+        return a
+    if a > b:
+        a, b = b, a
+    for i in range(a, b+1):
+        answer += i
+    return answer
