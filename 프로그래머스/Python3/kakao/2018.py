52392e9a08d3a41359d36407cc71730b50650407
ChoiByeol <honey333888@naver.com>
2022-07-22 20:07:17 +0900

    [프로그래머스] kakao - [1차] 다트 게임

--- 프로그래머스/Python3/kakao/2018.py
+++ 프로그래머스/Python3/kakao/2018.py
@@ -17,3 +17,34 @@ def solution(n, arr1, arr2):
                 tmp = ' ' + tmp
         answer.append(tmp)
     return answer
+
+# [1차] 다트 게임 level 1
+def solution(dartResult):
+    bonus_dict = {'S': 1, 'D': 2, 'T': 3}
+    answer = 0
+    frontNum = 0
+    num = ''
+    isNums = True
+    for dart in dartResult:
+        if dart.isdigit() == True:
+            if isNums == False:
+                answer += num
+                frontNum = num
+            if isNums == True:
+                num += dart
+            else:
+                num = dart
+            isNums = True
+        else:
+            num = int(num)
+            isNums = False
+            if dart in bonus_dict:
+                num = pow(num, bonus_dict[dart])
+            else:
+                if dart == '*':
+                    num = num * 2
+                    answer += frontNum
+                elif dart == '#':
+                    num = num * -1
+    answer += num
+    return answer
