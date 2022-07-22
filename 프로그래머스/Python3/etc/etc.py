88a0dd0da5472c1244be59e2a1ab51bf5a77dd7d
ChoiByeol <honey333888@naver.com>
2022-07-22 20:10:24 +0900

    [프로그래머스] 나누어 떨어지는 숫자 배열

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -158,3 +158,14 @@ def solution(price, money, count):
         return abs(answer)
     else:
         return 0
+
+# 나누어 떨어지는 숫자 배열 level 1
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    if answer == []:
+        answer.append(-1)
+    answer.sort()
+    return answer
