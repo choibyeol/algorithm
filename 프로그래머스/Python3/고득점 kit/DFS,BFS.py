bd7caed6860485ba8a0ba067cb0cfa09ff98093c
ChoiByeol <honey333888@naver.com>
2022-08-04 16:20:10 +0900

    [프로그래머스] DFS,BFS - 게임 맵 최단거리

--- 프로그래머스/Python3/고득점 kit/DFS,BFS.py
+++ 프로그래머스/Python3/고득점 kit/DFS,BFS.py
@@ -81,3 +81,34 @@ def solution(begin, target, words):
 
     dfs(w, target, answer, answers)
     return min(answers)
+
+# 게임 맵 최단거리 level 2
+from collections import deque
+
+def solution(maps):
+    answer = 0
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    n, m = len(maps), len(maps[0])
+    
+    def bfs(x, y):
+        queue = deque()
+        queue.append((x, y))
+        
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                    continue
+                if maps[nx][ny] == 0:
+                    continue
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx, ny))
+                
+    bfs(0, 0)
+    answer = maps[-1][-1]
+    if answer == 1:
+        return -1
+    return answer
