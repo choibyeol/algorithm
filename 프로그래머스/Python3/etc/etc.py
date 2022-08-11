f1305705cb48c161c5dcc6c317ec857851e669f1
ChoiByeol <honey333888@naver.com>
2022-08-11 21:05:42 +0900

    [프로그래머스] 약수의 합

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -228,3 +228,11 @@ def solution(seoul):
         if seoul[i] == "Kim":
             answer = "김서방은 " + str(i) + "에 있다"
     return answer
+
+# 약수의 합 level 1
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer += i
+    return answer
