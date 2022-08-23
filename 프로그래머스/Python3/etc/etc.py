ce504027e1344dac4ee7c380b76aa3953cf3bdf4
ChoiByeol <honey333888@naver.com>
2022-08-23 22:20:19 +0900

    [프로그래머스] 소수 찾기

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -258,3 +258,11 @@ def solution(n):
     for i in n:
         answer += int(i)
     return answer
+
+# 소수 찾기 level 1
+def solution(n):
+    num = set(range(2, n+1))
+    for i in range(2, n+1):
+        if i in num:
+            num -= set(range(2*i, n+1, i))
+    return len(num)
