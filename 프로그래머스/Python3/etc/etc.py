f23cfcfc15b6f59ad181736145af4fffb6d9095a
ChoiByeol <honey333888@naver.com>
2022-09-04 22:41:35 +0900

    [프로그래머스] 직사각형 별찍기

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -358,3 +358,10 @@ def solution(x, n):
         answer.append(x)
         x += num
     return answer
+
+# 직사각형 별찍기 level 1
+a, b = map(int, input().strip().split(' '))
+for i in range(b):
+    for j in range(a):
+        print('*', end="")
+    print()
