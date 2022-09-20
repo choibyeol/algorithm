dad8d623f36974fe1c03e0950aceb075ca3f344a
ChoiByeol <honey333888@naver.com>
2022-09-20 21:40:43 +0900

    [프로그래머스] 최고의 집합

--- 프로그래머스/Python3/etc/etc.py
+++ 프로그래머스/Python3/etc/etc.py
@@ -405,3 +405,20 @@ def solution(s):
         answer += tmp
         cnt += 1
     return answer
+
+# 최고의 집합 level 3
+def solution(n, s):
+    answer = []
+
+    base = s // n
+    rem = s % n
+
+    if base == 0:
+        return [-1]
+    
+    for _ in range(n - rem):
+        answer.append(base)
+    for _ in range(rem):
+        answer.append(base + 1)
+
+    return answer
