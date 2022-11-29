f0b8298167ffdf8d7144b7429714c21177af5b92
ChoiByeol <honey333888@naver.com>
2022-11-29 15:05:05 +0900

    [프로그래머스] kakao - [1차] 캐시

--- 프로그래머스/Python3/kakao/2018.py
+++ 프로그래머스/Python3/kakao/2018.py
@@ -48,3 +48,23 @@ def solution(dartResult):
                     num = num * -1
     answer += num
     return answer
+
+# [1차] 캐시 level 2
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            if len(cache) != cacheSize:
+                cache.append(city)
+            else:
+                cache.append(city)
+                cache.pop(0)
+            answer += 5
+
+    return answer
